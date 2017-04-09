6a9c1d2755abc9dfb0a7496de0dc7a0a63bbb07c
grrlove2017 <grrlove2017@hotmail.com>
2017-04-09 20:25:55 +0800

    C#多线程编程实战 加入第三章内容----使用线程池

---
+++ FirstGitProjects/BasicThreadPoolProject/Program.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace BasicThreadPoolProject
+{
+    class Program
+    {
+        private delegate string RunOnThreadPool(out int threadId);
+        static Timer _timer;
+
+        static void Main(string[] args)
+        {
+            #region 3.2 BeginInvoke、EndInvoke异步线程对，用于执行异步操作，以及完成异步操作之后的回调操作，其中Callback使用的是线程池
+
+            /*
+            int threadId = 0;
+
+            RunOnThreadPool poolDelegate = Test;
+
+            var t = new Thread(() =>Test(out threadId));
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thread Id: {0}", threadId);
+
+            IAsyncResult r = poolDelegate.BeginInvoke(out threadId, Callback, "a delegate asynchronous call");
+            r.AsyncWaitHandle.WaitOne();
+
+            string result = poolDelegate.EndInvoke(out threadId, r);
+
+            Console.WriteLine("Thread pool worker thread id: {0}", threadId);
+            Console.WriteLine(result);
+
+            Thread.Sleep(TimeSpan.FromSeconds(12));
+            */
+
+            #endregion
+
+            #region 3.3 通过线程池，加入异步操作
+
+            /*
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            ThreadPool.QueueUserWorkItem(AsyncOperation, "async state");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(state => {
+                Console.WriteLine("Operation state: {0}", state);
+                Console.WriteLine("Worker thread id: {0}",Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            */
+
+            #endregion
+
+            #region 3.4 比较创建线程和线程池中加入线程，使用线程，时间会短，但耗费的内存和线程数多，使用线程池时间会长，但是节省了内存和线程数
+
+            /*
+            const int numberOfOperations = 500;
+            var sw = new Stopwatch();
+            sw.Start();
+            UseThreads(numberOfOperations);
+            sw.Stop();
+            Console.WriteLine("Execution time using threads: {0}", sw.ElapsedMilliseconds);            
+
+            sw.Reset();
+            sw.Start();
+            UseThreadPool(numberOfOperations);
+            sw.Stop();
+            Console.WriteLine("Execution time using threads: {0}", sw.ElapsedMilliseconds);
+            */
+
+            #endregion
+
+            #region 3.5 取消任务
+
+            /*
+            using(var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation1(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            using (var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation2(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            using (var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation3(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            */
+
+            #endregion
+
+            #region 3.6 在线程池中使用等待事件处理器及超时
+
+            /*
+            RunOperations(TimeSpan.FromSeconds(5));
+            RunOperations(TimeSpan.FromSeconds(7));
+            */
+
+            #endregion
+
+            #region 3.7 使用计时器
+
+            /*
+            Console.WriteLine("Press 'Enter' to stop the timer...");
+            DateTime start = DateTime.Now;
+            _timer = new Timer(_ => TimerOperation(start), null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2));
+
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            _timer.Change(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(4));
+
+            Console.ReadLine();
+
+            _timer.Dispose();
+            */
+
+            #endregion
+
+            #region 3.8 BackgroundWorker 基于事件的异步模式 Event-based Asynchronous Pattern 简称EAP
+
+            var bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+
+            bw.DoWork += Worker_DoWork;
+            bw.ProgressChanged += Worker_ProgressChanged;
+            bw.RunWorkerCompleted += Worker_Completed;
+
+            bw.RunWorkerAsync();
+
+            Console.WriteLine("Press C to cancel work");
+            do
+            {
+                if (Console.ReadKey(true).KeyChar == 'C')
+                {
+                    bw.CancelAsync();
+                }
+            }
+            while (bw.IsBusy);
+
+            #endregion
+        }
+
+        static void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine("DoWork thread pool thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            var bw = (BackgroundWorker)sender;
+            for(int i=1;i<=100; i++)
+            {
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (i % 10 == 0)
+                {
+                    bw.ReportProgress(i);
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+            }
+            e.Result = 42;
+        }
+
+        static void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Console.WriteLine("{0}% completed. Progress thread pool thread id: {1}", e.ProgressPercentage, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static void Worker_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine("Completed thread pool thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            if (e.Error != null)
+            {
+                Console.WriteLine("Exception {0} has occured.", e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                Console.WriteLine("Operation has been canceled");
+            }
+            else
+            {
+                Console.WriteLine("The answer is: {0}", e.Result);
+            }
+        }
+
+        static void TimerOperation(DateTime start)
+        {
+            TimeSpan elapsed = DateTime.Now - start;
+            Console.WriteLine("{0} seconds from {1}. Timer thread pool thread id: {2}",elapsed.Seconds, start, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="workerOperationTimeout"></param>
+        static void RunOperations(TimeSpan workerOperationTimeout)
+        {
+            using(var evt = new ManualResetEvent(false))
+            {
+                using(var cts = new CancellationTokenSource())
+                {
+                    Console.WriteLine("Registering timeout operations...");
+                    //设置一个超时时间，以及超时或回调方法
+                    var worker = ThreadPool.RegisterWaitForSingleObject(evt, (state, isTimedOut) => WorkerOperationWait(cts, isTimedOut), null, workerOperationTimeout, true);
+
+                    Console.WriteLine("Starting long running operation...");
+
+                    ThreadPool.QueueUserWorkItem(_ => WorkerOperation(cts.Token, evt));
+
+                    Thread.Sleep(workerOperationTimeout.Add(TimeSpan.FromSeconds(2)));
+                    worker.Unregister(evt);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 设置一个6秒的操作
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="evt"></param>
+        static void WorkerOperation(CancellationToken token,ManualResetEvent evt)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            evt.Set();
+        }
+
+        /// <summary>
+        /// 超时或回调方法
+        /// </summary>
+        /// <param name="cts"></param>
+        /// <param name="isTimedOut"></param>
+        static void WorkerOperationWait(CancellationTokenSource cts, bool isTimedOut)
+        {
+            if (isTimedOut)
+            {
+                cts.Cancel();
+                Console.WriteLine("Worker operation timed out and was canceled.");
+            }
+            else
+            {
+                Console.WriteLine("Worker operation succeded");
+            }
+        }
+
+        /// <summary>
+        /// 检查IsCancellationRequested来判断是否被取消
+        /// </summary>
+        /// <param name="token"></param>
+        static void AsyncOperation1(CancellationToken token)
+        {
+            Console.WriteLine("Starting the first task");
+            for(int i = 0; i < 5; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("The first task has been canceled.");
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            Console.WriteLine("The second task has  completed successfully");
+        }
+
+        /// <summary>
+        /// 抛出一个OperationCancelledException异常
+        /// </summary>
+        /// <param name="token"></param>
+        static void AsyncOperation2(CancellationToken token)
+        {
+            try
+            {
+                Console.WriteLine("Starting the second task");
+
+                for(int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+                Console.WriteLine("The second task has completed successfully");
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("The second task has been canceled.");
+            }
+        }
+
+        /// <summary>
+        /// 注册一个回调函数，当取消操作时调用
+        /// </summary>
+        /// <param name="token"></param>
+        private static void AsyncOperation3(CancellationToken token)
+        {
+            bool cancellationFlag = false;
+            token.Register(() => cancellationFlag = true );
+            Console.WriteLine("Starting the third task");
+            for(int i = 0; i < 5; i++)
+            {
+                if (cancellationFlag)
+                {
+                    Console.WriteLine("The third task has been canceled.");
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            Console.WriteLine("The third task has completed successfully");
+        }
+
+        /// <summary>
+        /// 使用线程添加操作
+        /// </summary>
+        /// <param name="numberOfOperations"></param>
+        static void UseThreads(int numberOfOperations)
+        {
+            using(var countdown = new CountdownEvent(numberOfOperations))
+            {
+                Console.WriteLine("Scheduling work by creating threads");
+                for(int i = 0; i < numberOfOperations; i++)
+                {
+                    var thread = new Thread(() => {
+                        Console.WriteLine("{0},",Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                    thread.Start();
+                }
+                countdown.Wait();
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 使用线程池添加操作
+        /// </summary>
+        /// <param name="numberOfOperations"></param>
+        static void UseThreadPool(int numberOfOperations)
+        {
+            using(var countdown = new CountdownEvent(numberOfOperations))
+            {
+                for(int i = 0; i < numberOfOperations; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(_ => {
+                        Console.WriteLine("{0},", Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                }
+                countdown.Wait();
+                Console.WriteLine();
+            }
+        }
+
+        private static void AsyncOperation(object state)
+        {
+            Console.WriteLine("Operation state: {0}", state??"(null)");
+            Console.WriteLine("Worker thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        private static void Callback(IAsyncResult ar)
+        {
+            Console.WriteLine("Starting a callback...");
+            Console.WriteLine("State passed to a callback:{0}", ar.AsyncState);
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Console.WriteLine("Thread pool worker thread id: {0}",Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static string Test(out int threadId)
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return string.Format("Thread pool worker thread id was {0}", threadId);
+        }
+    }
+}
--- FirstGitProjects/ThreadSynchronization/Program.cs
+++ FirstGitProjects/ThreadSynchronization/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace ThreadSynchronization
@@ -13,6 +10,12 @@ namespace ThreadSynchronization
         private static AutoResetEvent _workerEvent = new AutoResetEvent(false);
         private static AutoResetEvent _mainEvent = new AutoResetEvent(false);
         static ManualResetEventSlim _mainSlimEvent = new ManualResetEventSlim(false);
+        static CountdownEvent _countdown = new CountdownEvent(2);
+        static Barrier _barrier = new Barrier(2, b => Console.WriteLine("End of phars {0}", b.CurrentPhaseNumber + 1));
+        static ReaderWriterLockSlim _rw = new ReaderWriterLockSlim();
+        static Dictionary<int, int> _items = new Dictionary<int, int>();
+        //volatile多用于多线程的环境，当一个变量定义为volatile时，读取这个变量的值时候每次都是从momery里面读取而不是从cache读。这样做是为了保证读取该变量的信息都是最新的
+        static volatile bool _isCompleted = false;
 
         static void Main(string[] args)
         {
@@ -57,8 +60,10 @@ namespace ThreadSynchronization
             #region 2.3.2 Muex互斥量
 
             /*当主程序启动时，定义一个指定名称的互斥量，设置initialOwner标志为false。意味着，如果互斥量被创建，则允许程序获取该互斥量。如果没有获取到互斥量，则走else.
-            //Mutex互斥量是全局的操作系统对象！可用于在不同的程序中同步线程。
+             *Mutex互斥量是全局的操作系统对象！可用于在不同的程序中同步线程。
+             */
 
+            /*
             const string MutexName = "CSharpThreadingCookbook";
 
             using(var m = new Mutex(false, MutexName))
@@ -97,7 +102,7 @@ namespace ThreadSynchronization
 
             #region 2.5.2 AutoResetEvent
 
-            /* 当主程序启动时，定义了两个AutoResetEvent实例。藏品睛个是从子线程向主线程发信号，另一个实例是从主线程向子线程发信号。我们向AutoResetEvent构造方法传入false，定义了这两个实例的初始状态为unsingaled。
+            /* 当主程序启动时，定义了两个AutoResetEvent实例。其中一个是从子线程向主线程发信号，另一个实例是从主线程向子线程发信号。我们向AutoResetEvent构造方法传入false，定义了这两个实例的初始状态为unsingaled。
              * 这意味着任何线程调用这两个对象中的任何一个的WaitOne方法将会被阻塞，直到我们调用了Set方法。如果初始事件状态为true，那么AutoResetEvent实例的状态为signaled，如果线程调用WaitOne方法则会被立即处理。
              * 然后事件状态自动变为unsignaled，所以需要再对该实例调用一次Set方法，以便让其他的线程对该实例调用WaitOne方法从而继续执行。
              * 使用的是内核时间模式，所以等待时间不能太长。
@@ -117,28 +122,188 @@ namespace ThreadSynchronization
 
             #endregion
 
-            #region 2.6.2 ManualResetEventSlim
+            #region 2.6.2 ManualResetEventSlim 整个工作方式有点像人群通过大门
 
-            var t1 = new Thread(() => TravelThroughGates("Thread 1", 5));
-            var t2 = new Thread(() => TravelThroughGates("Thread 2", 6));
-            var t3 = new Thread(() => TravelThroughGates("Thread 3", 12));
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            Thread.Sleep(TimeSpan.FromSeconds(6));
-            Console.WriteLine("The gates are now open!");
-            _mainSlimEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            _mainSlimEvent.Reset();
-            Console.WriteLine("The gates have been closed!");
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            Console.WriteLine("The gates are now open for the second time!");
-            _mainSlimEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            Console.WriteLine("The gates have been closed!");
-            _mainSlimEvent.Reset();
+            /* ManualResetEventSlim的整个工作方式有点像人群通过大门。一直保持大门敞开直到手动调用Reset方法。
+             * 当调用Set时，相当于打开了大门从而允许准备好的线程接收信号并继续工作
+             */
+            /*
+           var t1 = new Thread(() => TravelThroughGates("Thread 1", 5));
+           var t2 = new Thread(() => TravelThroughGates("Thread 2", 6));
+           var t3 = new Thread(() => TravelThroughGates("Thread 3", 12));
+           t1.Start();
+           t2.Start();
+           t3.Start();
+           Thread.Sleep(TimeSpan.FromSeconds(6));
+           Console.WriteLine("The gates are now open!");
+           _mainSlimEvent.Set();
+           Thread.Sleep(TimeSpan.FromSeconds(2));
+           _mainSlimEvent.Reset();
+           Console.WriteLine("The gates have been closed!");
+           Thread.Sleep(TimeSpan.FromSeconds(10));
+           Console.WriteLine("The gates are now open for the second time!");
+           _mainSlimEvent.Set();
+           Thread.Sleep(TimeSpan.FromSeconds(2));
+           Console.WriteLine("The gates have been closed!");
+           _mainSlimEvent.Reset();
+           */
 
             #endregion
+
+            #region 2.7.2 CountDownEvent 可以调用多个线程，并且等待返回信号，如果有线程没有返回信号，则会继续等待
+
+            /*
+            Console.WriteLine("Starting two operations");
+            var t1 = new Thread(() => PerformOperation("Operation 1 is completed",4));
+            var t2 = new Thread(() => PerformOperation("Operation 2 is completed", 8));
+            t1.Start();
+            t2.Start();
+            _countdown.Wait();
+            Console.WriteLine("Both operations have been completed.");
+            _countdown.Dispose();
+            */
+
+            #endregion
+
+            #region 2.8.2 Barrier 用于组织多个线程及时在某个时刻碰面 多线程迭代计算时非常有用，可以在每个迭代结束前执行一些计算
+
+            /*
+            var t1 = new Thread(() => PlayMusic("the guitarist", "play an amazing solo",5));
+            var t2 = new Thread(() => PlayMusic("the singer", "sing his song",2));
+            t1.Start();
+            t2.Start();
+            */
+
+            #endregion
+
+            #region 2.9.2 ReaderWriterLockSlim 允许多个线程同时读取，以及独占写
+
+            /*
+            new Thread(Read) { IsBackground = true }.Start();
+            new Thread(Read) { IsBackground = true }.Start();
+            new Thread(Read) { IsBackground = true }.Start();
+
+            new Thread(() => Write("Thread 1")) { IsBackground=true }.Start();
+            new Thread(() => Write("Thread 2")) { IsBackground = true }.Start();
+
+            Thread.Sleep(TimeSpan.FromSeconds(30));
+            */
+
+            #endregion
+
+            #region 2.10.2 SpinWait 用户模式使线程等待
+
+            var t1 = new Thread(UserModeWait);
+            var t2 = new Thread(HybridSpinWait);
+
+            Console.WriteLine("Running user mode waiting");
+            t1.Start();
+            Thread.Sleep(20);
+            _isCompleted = true;
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            _isCompleted = false;
+            Console.WriteLine("Running hybrid SpinWait construct waiting");
+            t2.Start();
+            Thread.Sleep(5);
+            _isCompleted = true;
+
+            #endregion
+        }
+
+        static void UserModeWait()
+        {
+            while (!_isCompleted)
+            {
+                Console.WriteLine(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Waiting is complete");
+        }
+
+        static void HybridSpinWait()
+        {
+            var w = new SpinWait();
+            while (!_isCompleted)
+            {
+                w.SpinOnce();
+                Console.WriteLine(w.NextSpinWillYield);
+            }
+            Console.WriteLine("Waiting is complete");
+        }
+
+        static void Read()
+        {
+            Console.WriteLine("Reading contents of a dictionary");
+            while (true)
+            {
+                try
+                {
+                    _rw.EnterReadLock();
+                    foreach(var key in _items.Keys)
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    }
+                }
+                finally
+                {
+                    _rw.ExitReadLock();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 当发现需要修改时用EnterWriteLock获取写锁写入数据
+        /// </summary>
+        /// <param name="threadName"></param>
+        static void Write(string threadName)
+        {
+            while (true)
+            {
+                try
+                {
+                    int newKey = new Random().Next(250);
+                    //读写锁
+                    _rw.EnterUpgradeableReadLock();
+                    if (!_items.ContainsKey(newKey))
+                    {
+                        try
+                        {
+                            _rw.EnterWriteLock();
+                            _items[newKey] = 1;
+                            Console.WriteLine("New key {0} is added to a dictionary by a {1}",newKey,threadName);
+                        }
+                        finally
+                        {
+                            _rw.ExitWriteLock();
+                        }
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                }
+                finally
+                {
+                    _rw.ExitUpgradeableReadLock();
+                }
+            }
+        }
+
+        static void PlayMusic(string name,string message,int seconds)
+        {
+            for(int i = 1; i < 3; i++)
+            {
+                Console.WriteLine("---------------------------------");
+                Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                Console.WriteLine("{0} starts to {1}", name, message);
+                Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                Console.WriteLine("{0} finishes to {1}", name, message);
+                _barrier.SignalAndWait();
+            }
+        }
+
+        static void PerformOperation(string message, int seconds)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine(message);
+            _countdown.Signal();
         }
 
         static void TravelThroughGates(string threadName, int seconds)
