820a181828095c181e00b856d504c6ba1af17dcf
grrlove2017 <grrlove2017@hotmail.com>
2017-04-10 23:15:34 +0800

    C#多线程编程实战 使用任务并行库

--- FirstGitProjects/TaskParallelLibraryProject/Program.cs
+++ FirstGitProjects/TaskParallelLibraryProject/Program.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.ComponentModel;
 
 namespace TaskParallelLibraryProject
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             #region 4.2 创建Task
@@ -104,10 +106,171 @@ namespace TaskParallelLibraryProject
 
             #region 4.5 将APM模式转换为任务
 
+            /*
+            int threadId;
+            AsynchronousTask d = Test;
+            IncompatibleAsynchronousTask e = Test;
 
+            Console.WriteLine("Option 1");
+            Task<string> task = Task<string>.Factory.FromAsync(d.BeginInvoke("AsyncTaskThread", Callback, "a delegate asynchronous call"), d.EndInvoke);
+
+            task.ContinueWith(t => Console.WriteLine("Callback is finished, now running a continuation! Result: {0}", t.Result));
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+            Console.WriteLine(task.Status);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Option 2");
+
+            task = Task<string>.Factory.FromAsync(d.BeginInvoke, d.EndInvoke, "AsyncTaskThread", "a delegate asynchronous call");
+            task.ContinueWith(t => Console.WriteLine("Task is completed, now running a continuation! Result: {0}", t.Result));
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+            Console.WriteLine(task.Status);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Option 3");
+
+            IAsyncResult ar = e.BeginInvoke(out threadId, Callback, "a delegate asynchronous call");
+            ar = e.BeginInvoke(out threadId, Callback, "a delegate asynchronous call");
+            task = Task<string>.Factory.FromAsync(ar, _ => e.EndInvoke(out threadId, ar));
+            task.ContinueWith(t => Console.WriteLine("Task is completed, now running a continuation! Result:{0}, ThreadId: {1}", t.Result,threadId));
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+            Console.WriteLine(task.Status);
+            Thread.Sleep(TimeSpan.FromSeconds(100));
+            */
 
             #endregion
 
+            #region 4.6 将EAP模式转换为任务
+
+            /*
+            var tcs = new TaskCompletionSource<int>();
+
+            var worker = new BackgroundWorker();
+            worker.DoWork += (sender, eventArgs) =>
+            {
+                eventArgs.Result = TaskMethodForEAP("Background worker", 5);
+            };
+
+            worker.RunWorkerCompleted += (sender, eventArgs) =>
+            {
+                if (eventArgs.Error != null)
+                {
+                    tcs.SetException(eventArgs.Error);
+                }
+                else if (eventArgs.Cancelled)
+                {
+                    tcs.SetCanceled();
+                }
+                else
+                {
+                    tcs.SetResult((int)eventArgs.Result);
+                }
+            };
+
+            worker.RunWorkerAsync();
+
+            int result = tcs.Task.Result;
+
+            Console.WriteLine("Result is: {0}",result);
+            */
+
+            #endregion
+
+            #region 4.7 实现取消选项
+
+            var cts = new CancellationTokenSource();
+            var longTask = new Task<int>(() => TaskMethodForCancel("Task 1", 10, cts.Token), cts.Token);
+            Console.WriteLine(longTask.Status);
+            cts.Cancel();
+            Console.WriteLine(longTask.Status);
+            Console.WriteLine("First task has been cancelled befor execution");
+            cts = new CancellationTokenSource();
+            longTask = new Task<int>(() => TaskMethodForCancel("Task 2", 10, cts.Token), cts.Token);
+            longTask.Start();
+            for(int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine(longTask.Status);
+            }
+            cts.Cancel();
+            for(int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine(longTask.Status);
+            }
+
+            Console.WriteLine("A task has been completed with result {0}.", longTask.Result);
+
+            #endregion
+
+        }
+
+        private static int TaskMethodForCancel(string name, int seconds, CancellationToken token)
+        {
+            Console.WriteLine("Task {0} is running on a thread id: {1}. Is thread pool thread: {2}", name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            for(int i = 0; i < seconds; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                if (token.IsCancellationRequested)
+                {
+                    return -1;
+                }
+            }
+            return 42 * seconds;
+        }
+
+        static int TaskMethodForEAP(string name, int seconds)
+        {
+            Console.WriteLine("Task {0} is running on a thread id: {1}. Is thread pool thread: {2}", name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            return 42 * seconds;
+        }
+
+        private delegate string AsynchronousTask(string threadName);
+        private delegate string IncompatibleAsynchronousTask(out int threadId);
+
+        private static void Callback(IAsyncResult ar)
+        {
+            Console.WriteLine("Starting a callback...");
+            Console.WriteLine("State passed to a callback: {0}", ar.AsyncState);
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Console.WriteLine("Thread pool worker thread id: {0}",Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static string Test(string threadName)
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Thread.CurrentThread.Name = threadName;
+            return string.Format("Thread name: {0}", Thread.CurrentThread.Name);
+        }
+
+        private static string Test(out int threadId)
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return string.Format("Thread pool worker thread id was: {0}", threadId);
         }
 
         static int TaskMethodCombination(string name, int seconds)
